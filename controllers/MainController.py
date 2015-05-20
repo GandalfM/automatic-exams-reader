390e74afac86a97c3e712255352233bbb0bbe5c8
Adrian Boguszewski <adrbogus1@stuent.pg.gda.pl>
2015-05-20 22:15:29 +0200

    Added simple main window

---
+++ controllers/MainController.py
@@ -0,0 +1,7 @@
+from PyQt5.QtWidgets import QMainWindow
+cd 
+class MainController(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.show()
---
+++ main.py
@@ -0,0 +1,9 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+from controllers.MainController import MainController
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    app.setApplicationName("Automatic Exams Reader")
+    aer = MainController()
+    sys.exit(app.exec())
