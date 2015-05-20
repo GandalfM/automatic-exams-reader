4a44c7c39e9ea7843424810aff260cb57da765ed
Adrian Boguszewski <adrbogus1@student.pg.gda.pl>
2015-05-21 00:24:35 +0200

    Added script to translate ui files

---
+++ convert-ui.py
@@ -0,0 +1,8 @@
+import os
+
+__author__ = 'adek'
+
+os.chdir("views")
+for file in os.listdir("."):
+    if file.endswith(".ui"):
+        os.system("pyuic5 {} -o {}".format(file, file.replace(".ui", ".py")))
